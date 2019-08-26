0d34b4725889aa7b860874cfba8a70ff3caa88c4
David Lemarier <david@lemarier.ca>
2019-08-26 17:08:55 -0400

    Integrate PubSub and Bull

---
+++ README.md
@@ -0,0 +1,23 @@
+# Oryx Packages
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-green.svg)](LICENSE.md)
+[![lerna](https://img.shields.io/badge/maintained%20with-lerna-cc00ff.svg)](https://lernajs.io/)
+
+A loosely related set of packages for JavaScript / TypeScript projects at Oryx Commerce.
+
+## Usage
+
+The repo is managed as a monorepo that is composed of many npm packages.
+Each package has its own `README` and documentation describing usage.
+
+### Package Index
+
+| package |     |     |
+| ------- | --- | --- |
+| address | [directory](packages/scheduler) | [![npm version](https://badge.fury.io/js/%40oryxcommerce%2Fscheduler.svg)](https://badge.fury.io/js/%40oryxcommerce%2Fscheduler)
+
+## License
+
+MIT &copy; [Shopify](https://shopify.com/), see [LICENSE.md](LICENSE.md) for details.
+
+<a href="http://www.shopify.com/"><img src="https://cdn.shopify.com/assets2/press/brand/shopify-logo-main-small-f029fcaf14649a054509f6790ce2ce94d1f1c037b4015b4f106c5a67ab033f5b.png" alt="Shopify" width="200" /></a>
--- packages/scheduler/index.js
+++ packages/scheduler/index.js
@@ -1,58 +1,11 @@
-const Queue = require("bull");
-const { PubSub } = require("@google-cloud/pubsub");
-const humanInterval = require("human-interval");
+const Queue = require('bull');
+const {PubSub} = require('@google-cloud/pubsub');
+const humanInterval = require('human-interval');
 
-const postMessage = async ({
-  data,
-  options = {
-    delay: 0,
-    pubsub: {
-      projectId: null,
-      topic: null
-    },
-    bull: {
-      queueName: null,
-      redis: "redis://127.0.0.1:6379"
-    }
-  }
-}) => {
-  let { delay } = options;
-  const { projectId, topic } = options.pubsub;
-  const { redis: server, queueName } = options.bull;
-
-  // Convert delay to human readable
-  delay = humanInterval(delay) || 0;
-
-  // should be scheduled inside bull
-  if (delay > 0) {
-    // we add the topic inside the job
-    const realData = { ...data, topic };
-    const bullData = await sendToBull({
-      queueName,
-      server,
-      data: realData,
-      delay
-    });
-    return { status: "success", errors: null, type: "bull", debug: bullData };
-  } else {
-    // send directly to PubSub
-    const pubSubData = await sendToPubSub({ projectId, topic, data });
-    return {
-      status: "success",
-      errors: null,
-      type: "pubsub",
-      debug: { id: pubSubData }
-    };
-  }
-
-  // make sure to return at least an empty json
-  return { status: "failed", errors: "Unknown error" };
-};
-
-const sendToBull = async ({ queueName, data, server, delay }) => {
+const sendToBull = async ({queueName, data, server, delay}) => {
   // should be scheduled inside bull
   if (delay === 0) {
-    throw new Error("Please use PubSub instead");
+    throw new Error('Please use PubSub instead');
   }
 
   // Create our queue instance
@@ -60,7 +13,7 @@ const sendToBull = async ({ queueName, data, server, delay }) => {
 
   // Submit job to the queue
   const bullData = await queue.add(data, {
-    delay
+    delay,
   });
 
   // Make sure the queue is closed
@@ -70,16 +23,64 @@ const sendToBull = async ({ queueName, data, server, delay }) => {
   return bullData || {};
 };
 
-const sendToPubSub = async ({ projectId, topic, data }) => {
-  const pubsub = new PubSub({ projectId });
+const sendToPubSub = async ({projectId, topic, data}) => {
+  const pubsub = new PubSub({projectId});
   const dataBuffer = Buffer.from(JSON.stringify(data));
   const dataReturned = await pubsub.topic(topic).publish(dataBuffer);
 
   return dataReturned || {};
 };
 
+const postMessage = async ({
+  data,
+  options = {
+    delay: 0,
+    pubsub: {
+      projectId: null,
+      topic: null,
+    },
+    bull: {
+      queueName: null,
+      redis: 'redis://127.0.0.1:6379',
+    },
+  },
+}) => {
+  let {delay} = options;
+  const {projectId, topic} = options.pubsub;
+  const {redis: server, queueName} = options.bull;
+
+  // Convert delay to human readable
+  delay = humanInterval(delay) || 0;
+  try {
+    // should be scheduled inside bull
+    if (delay > 0) {
+      // we add the topic inside the job
+      const realData = {...data, topic};
+      const bullData = await sendToBull({
+        queueName,
+        server,
+        data: realData,
+        delay,
+      });
+      return {status: 'success', errors: null, type: 'bull', debug: bullData};
+    } else {
+      // send directly to PubSub
+      const pubSubData = await sendToPubSub({projectId, topic, data});
+      return {
+        status: 'success',
+        errors: null,
+        type: 'pubsub',
+        debug: {id: pubSubData},
+      };
+    }
+  } catch (error) {
+    // make sure to return at least an empty json
+    return {status: 'failed', errors: error.type, debug: error};
+  }
+};
+
 module.exports = {
   postMessage,
   sendToBull,
-  sendToPubSub
+  sendToPubSub,
 };
